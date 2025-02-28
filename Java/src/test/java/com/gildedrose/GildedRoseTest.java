449f925333ab84ba949377db5133231458ea5b06
Salvador Perez Martin <salva.perezmartin@telefonica.com>
2025-02-28 15:42:31 +0100

    Add java tests for the workshop

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,36 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    private static Stream<Arguments> gildedRoseTestSource() {
+        return Stream.of(
+            Arguments.of("Arbitrary item", 2, 6, 1, 5),
+            Arguments.of("Arbitrary item", 0, 6, -1, 4),
+            Arguments.of("Aged Brie", 2, 0, 1, 1),
+            Arguments.of("Aged Brie", 0, 1, -1, 3),
+            Arguments.of("Aged Brie", 0, 50, -1, 50),
+            Arguments.of("Sulfuras, Hand of Ragnaros", 0, 80, 0, 80),
+            Arguments.of("Backstage passes to a TAFKAL80ETC concert", 10, 5, 9, 7),
+            Arguments.of("Backstage passes to a TAFKAL80ETC concert", 5, 5, 4, 8),
+            Arguments.of("Backstage passes to a TAFKAL80ETC concert", 0, 5, -1, 0)
+        );
     }
 
+    @ParameterizedTest
+    @MethodSource("gildedRoseTestSource")
+    void SulfurasRemainsUnchanged(String name, int startingSellin, int startingQuality, int endSellin, int endQuality) {
+        Item sulfuras = new Item(name, startingSellin, startingQuality);
+        GildedRose app = new GildedRose(new Item[] { sulfuras });
+        app.updateQuality();
+        assertEquals(endQuality, sulfuras.quality);
+        assertEquals(endSellin, sulfuras.sellIn);
+    }
 }
